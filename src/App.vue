f8e7cfac95a047a6bb2263a4621fe87ade09a800
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-28 03:05:47 +0530

    CRUD Todo - Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-// import OptionsApp from './components/OptionsApp.vue';
-// import CompositionAppLong from './components/CompositionAppLong.vue';
-// import CompositionAppShort from './components/CompositionAppShort.vue';
-// import FormComposition from './components/FormComposition.vue';
-import FormTodoList from './components/FormTodoList.vue';
+
+  // import OptionsApp from './components/OptionsApp.vue';
+  // import CompositionAppLong from './components/CompositionAppLong.vue';
+  // import CompositionAppShort from './components/CompositionAppShort.vue';
+  // import FormComposition from './components/FormComposition.vue';
+  import FormTodoList from './components/FormTodoList.vue';
+
 </script>
 
 <template>
--- src/components/FormTodoList.vue
+++ src/components/FormTodoList.vue
@@ -1,20 +1,46 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const newTodo = ref('');
-const todos = ref([]);
+const taskInput = ref('')
+const taskList = ref(['111', '222'])
+const editingIndex = ref(null)
 
-const addTodo = () => {
-    todos.value.push(newTodo.value);
-    newTodo.value = '';
+const addItem = () => {
+  if (!taskInput.value.trim()) return
+
+  if (editingIndex.value !== null) {
+    taskList.value[editingIndex.value] = taskInput.value
+    editingIndex.value = null
+  } else {
+    taskList.value.push(taskInput.value)
+  }
+
+  taskInput.value = ''
+}
+
+const deleteItem = (item) => {
+  taskList.value = taskList.value.filter(el => el !== item)
+}
+
+const updateItem = (task) => {
+  editingIndex.value = taskList.value.indexOf(task)
+  taskInput.value = task
 }
 </script>
+
 <template>
-    <form>
-        <input type="text" v-model="newTodo" />
-        <button @click.prevent="addTodo">Add</button>
-    </form>
-    <ul>
-        <li v-for="todo in todos" :key="todo">{{ todo }}</li>
-    </ul>
+  <form @submit.prevent="addItem">
+    <input type="text" v-model="taskInput" />
+    <button>
+      {{ editingIndex !== null ? 'Update' : 'Add' }}
+    </button>
+  </form>
+
+  <ul>
+    <li v-for="(task, index) in taskList" :key="index">
+      {{ task }}
+      <button @click="deleteItem(task)">Delete</button>
+      <button @click="updateItem(task)">Update</button>
+    </li>
+  </ul>
 </template>
