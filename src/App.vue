456b2dc92a3bff4da5bbe107ae27cd807b0ef184
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-27 14:17:48 +0530

    For loop

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ export default {
   data() {
     return {
       title: 'Vue.Js',
-      status: "Inactive"
+      status: "Inactive",
+      tasks: ['task one', 'task two', 'task three']
     }
   }
 }
@@ -12,8 +13,12 @@ export default {
 <template>
   <p>{{ title }}</p>
 
-  <p v-if="status==='Active'">Active</p>
-  <p v-else-if="status==='Inactive'">Inactive</p>
+  <p v-if="status === 'Active'">Active</p>
+  <p v-else-if="status === 'Inactive'">Inactive</p>
   <p v-else>Unknown</p>
 
+  <h2>Tasks</h2>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
