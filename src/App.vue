492c8ee072f9ba1a2c4097eee939a383a845c9fa
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-28 02:27:00 +0530

    ToDO List

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 // import OptionsApp from './components/OptionsApp.vue';
 // import CompositionAppLong from './components/CompositionAppLong.vue';
 // import CompositionAppShort from './components/CompositionAppShort.vue';
-import FormComposition from './components/FormComposition.vue';
+// import FormComposition from './components/FormComposition.vue';
+import FormTodoList from './components/FormTodoList.vue';
 </script>
 
 <template>
   <!-- <OptionsApp /> -->
   <!-- <CompositionAppLong /> -->
   <!-- <CompositionAppShort /> -->
-   <FormComposition />
+  <!-- <FormComposition /> -->
+  <FormTodoList />
 </template>
---
+++ src/components/FormTodoList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTodo = ref('');
+const todos = ref([]);
+
+const addTodo = () => {
+    todos.value.push(newTodo.value);
+    newTodo.value = '';
+}
+</script>
+<template>
+    <form>
+        <input type="text" v-model="newTodo" />
+        <button @click.prevent="addTodo">Add</button>
+    </form>
+    <ul>
+        <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+    </ul>
+</template>
