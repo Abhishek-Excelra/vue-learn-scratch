da19157fdad68fc3e0ef1582eb4395169ce9ca7f
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-28 15:37:06 +0530

    Fetch users using onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-
-  // import OptionsApp from './components/OptionsApp.vue';
+  // import OptionsApp from './compone /OptionsApp.vue';
   // import CompositionAppLong from './components/CompositionAppLong.vue';
   // import CompositionAppShort from './components/CompositionAppShort.vue';
   // import FormComposition from './components/FormComposition.vue';
-  import FormTodoList from './components/FormTodoList.vue';
+  // import FormTodoList from './components/FormTodoList.vue';
+  import FetchUsers from './components/FetchUsers.vue';
 
 </script>
 
@@ -12,6 +12,9 @@
   <!-- <OptionsApp /> -->
   <!-- <CompositionAppLong /> -->
   <!-- <CompositionAppShort /> -->
-  <!-- <FormComposition /> -->
-  <FormTodoList />
+  <!-- <FormCompositi con /> -->
+  <!-- <FormTodoList /> -->
+   <FetchUsers />
 </template>
+
+
---
+++ src/components/FetchUsers.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+ const users = ref([])
+
+ onMounted(async()=>{
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await response.json()
+  users.value = data.map(el=>el.name)
+ })
+
+</script>
+<template>
+  <h2>Fetch Users Name</h2>
+  <ul>
+    <li v-for="value in users" :key="value">{{ value }}</li>
+  </ul>
+</template>
+
